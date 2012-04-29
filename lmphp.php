754239a5874d38d6bba9c24f41bfa8fa5a7fe3d1
Yu Onda <Yunda@Yundas-MacBook-Pro.local>
2012-04-29 20:27:32 +0900

    displaying bus time -10min to +30min of current time
    done just for KAKUTEI for now

--- lmphp.php
+++ lmphp.php
@@ -13,19 +13,61 @@
 <?php
 //php_language marathon
 
+//set timezone
+date_default_timezone_set('Asia/Tokyo');
+
+//set time
+$now = new DateTime(now);
+$prevbus = new DateTime(now);
+$nextbus = new DateTime(now);
+
+//display current time
+echo "現在時刻　";
+echo $now->format('G:i'); 
+echo "<br>";
+
+echo "<br>";
+
+//timetable to get
+echo "10分前（";
+$prevbus->modify('-10 minutes');
+echo $prevbus->format('G:i')."）までのバスと"; 
+
+echo "<br>";
+
+echo "30分後（";
+$nextbus->modify('+30 minutes');
+echo $nextbus->format('G:i')."）までのバスを表示"; 
+echo "<br>";
+
+echo "<br>";
+
+
+
 //get contents -> array
 $regbus = file_get_contents("regbustable.txt");
 $twinbus = file_get_contents("twinbustable.txt");
 
 
-//split array by space -> array
+//split array by space
 $split_regbus = explode(' ', $regbus);
 $split_twinbus = explode(' ', $twinbus);
 
 //display by line
 echo "各停<br>";
+
 foreach ($split_regbus as $single_regbus) {
-    echo $single_regbus."<br>";
+    //split hour:time
+    $regbus_t = explode(":", $single_regbus);
+    $time = $regbus_t[0].":".$regbus_t[1];
+    
+    //save as time
+    $bustime = new DateTime($time);
+    
+    //display if bus time is -10min or +30min from now
+    if($bustime >= $prevbus && $bustime <= $nextbus){
+    echo $bustime->format('G:i')."<br>";
+    }
 }
 
 echo "<br>";
