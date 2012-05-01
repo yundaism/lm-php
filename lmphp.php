d5512f5ad3212f28f81c2ab87a95a1eafe660653
Yu Onda <Yunda@Yundas-MacBook-Pro.local>
2012-05-01 22:52:59 +0900

    made it able to set displaying time duration
    also added counter to show notification if there is no bus at the time

--- lmphp.php
+++ lmphp.php
@@ -9,7 +9,6 @@
 
 <body>
 
-
 <?php
 //php_language marathon
 
@@ -28,30 +27,36 @@ echo "<br>";
 
 echo "<br>";
 
+//insert displaying bus time duration in minutes
+$minus = 10;
+$plus = 30;
+
 //timetable to get
-echo "10分前（";
-$prevbus->modify('-10 minutes');
+echo $minus."分前（";
+$prevbus->modify('-'.$minus.' minutes');
 echo $prevbus->format('G:i')."）までのバスと"; 
 
 echo "<br>";
 
-echo "30分後（";
-$nextbus->modify('+30 minutes');
+echo $plus."分後（";
+$nextbus->modify('+'.$plus.' minutes');
 echo $nextbus->format('G:i')."）までのバスを表示"; 
 echo "<br>";
 
 echo "<br>";
 
 
-
 //get contents -> array
 $regbus = file_get_contents("regbustable.txt");
 $twinbus = file_get_contents("twinbustable.txt");
 
-
 //split array by space
 $split_regbus = explode(' ', $regbus);
 $split_twinbus = explode(' ', $twinbus);
+
+//displayed bus time counter
+$count_r = 0;
+$count_t = 0;
 
 //time for local bus
 echo "各停<br>";
@@ -63,8 +68,15 @@ foreach ($split_regbus as $single_regbus) {
     
     //display if bus time is -10min or +30min from now
     if($bustime >= $prevbus && $bustime <= $nextbus){
-    echo $bustime->format('G:i')."<br>";
+        echo $bustime->format('G:i')."<br>";
+        $count_r++;
     }
+    
+}
+
+//if there is no bus for the 40min
+if($count_r == 0){
+    echo "指定された時刻にはバスの運行がありません。<br>";
 }
 
 //time for twinliner
@@ -72,16 +84,17 @@ echo "<br>";
 echo "ツインライナー<br>";
 foreach ($split_twinbus as $single_twinbus) {
 
-    //save as time
     $bustime_t = new DateTime($single_twinbus);
-    
-    //display if bus time is -10min or +30min from now
+
     if($bustime_t >= $prevbus && $bustime_t <= $nextbus){
-    echo $bustime_t->format('G:i')."<br>";
+        echo $bustime_t->format('G:i')."<br>";
+        $count_t++;
     }
 }
 
-
+if($count_t == 0){
+    echo "指定された時刻にはバスの運行がありません。";
+}
 
 ?>
 
